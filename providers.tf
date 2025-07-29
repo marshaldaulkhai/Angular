5ef62c6def1b0980355815ace4921bead2190cd9
marshaldaulkhai <MARSHALDAULKHAI@GMAIL.COM>
2025-07-29 14:01:10 +0530

    Delete providers.tf

--- providers.tf
+++
@@ -1,8 +0,0 @@
-# providers.tf
-#
-# This file configures the Azure provider for the angular-webapp module.
-
-provider "azurerm" {
-  features {} # This block enables the Azure Provider Features configuration.
-              # It's a best practice to include it.
-}
